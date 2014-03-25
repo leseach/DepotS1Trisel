cf70a45344b4dc5e563b66f71a5c0e05ad9b7d4f
lequay-l <liliane.lequay@saint-louis29.net>
2014-03-25 14:32:34 +0100

    envoi 1

---
+++ ProjetJava/src/com/vue/ProgPrin.java
@@ -0,0 +1,13 @@
+package com.vue;
+
+public class ProgPrin {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("test retour repository");
+
+	}
+
+}
---
+++ TestSourceForge/src/com/vue/ProgPrin.java
@@ -0,0 +1,14 @@
+package com.vue;
+
+public class ProgPrin {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("tttt");
+		
+
+	}
+
+}
